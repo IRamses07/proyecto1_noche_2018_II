a88a2e8ad4e36180bed3c3ced0a6ce42a63ef7da
pabskun <pablo.monestel@gmail.com>
2018-06-25 19:14:09 -0600

    commit
    Agregado petición entre public y api

---
+++ semana8/arquitectura/api/components/users/users.route.js
@@ -0,0 +1,18 @@
+'use strict';
+const express = require('express');
+const router = express.Router();
+const users = require('./users.api');
+
+
+router.route('/registrar_usuario')
+    .post(function(req, res){
+    users.registrar(req, res);
+});
+
+router.route('/listar_usuarios')
+    .get(function(req, res){
+    users.listar(req, res);
+});
+
+
+module.exports = router;
